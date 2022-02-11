b7e80da1819703268f46970714d8d29eb5e01907
Shannon Bradshaw <shannon.bradshaw@arrikto.com>
2022-02-11 11:04:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,12 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          type="text/javascript"
+          src="https://sdk.relicx.ai/relicx-sdk.min.js"
+          onload="relicxSDK.init({orgID: '96722432-95dc-466c-8b62-e9e7ad4b276f',appID: 'df69a78c-0f83-4134-811d-fd54b8e51df9'})">
+        </script>
+
       </Head>
 
       <main>
